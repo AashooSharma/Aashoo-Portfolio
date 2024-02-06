9510c8dccfa07c5002a0e01cdf849e060ebc7cbb
As Hacker <65814523+AashooSharma@users.noreply.github.com>
2024-02-07 00:00:02 +0530

    Update certificates.js

--- certificates/certificates.js
+++ certificates/certificates.js
@@ -71,3 +71,24 @@ async function showCertificates() {
 
 // Call the function to display certificates on page load
 showCertificates();
+
+
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
