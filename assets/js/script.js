3f319369b623a25ac648bbee14d6deea12f22fea
As Hacker <65814523+AashooSharma@users.noreply.github.com>
2024-04-16 16:46:22 +0530

    Update script.js
    tawk to configuration updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -350,6 +350,7 @@ document.onkeydown = function (e) {
 
 // <!--Start of Tawk.to Script-->
 {/* <script type="text/javascript"> */}
+/*
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -359,10 +360,23 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
 })();
+*/
 // </script>
 // <!--End of Tawk.to Live Chat with Aashoo Script-->
 
-
+<!--Start of Tawk.to Script-->
+//<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/661e5cb21ec1082f04e2fb6a/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+//</script>
+<!--End of Tawk.to Script-->
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
